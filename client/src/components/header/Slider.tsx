8f65f47b74084a24cfac4d8dbc7ef15fbddeced5
Jaesung Park <richdad6208@gmail.com>
2023-09-11 18:11:17 +0900

    이미지 슬라이드 구현중

--- client/src/components/header/Slider.tsx
+++ client/src/components/header/Slider.tsx
@@ -3,16 +3,10 @@ import { Section, SliderWrapper } from "../../style/slider";
 import { sleep } from "../../library/sleep";
 
 function Slider() {
-  const [slideNumber, setSlideNumber] = useState(1);
-
-  useEffect(() => {
-    sleep(1000).then();
-    setSlideNumber((prev) => prev + 1);
-  }, [slideNumber]);
   return (
     <Section>
       <SliderWrapper>
-        <img src={`./images/slide-0${slideNumber}.jpg`} alt="" />
+        {/* <img src={`./images/slide-0${slideNumber}.jpg`} alt="" /> */}
       </SliderWrapper>
     </Section>
   );
